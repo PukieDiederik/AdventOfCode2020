c8c9b5df60b232ddc5d78a041ac4057a837b758e
PukieDiederik <diederik.boeren@gmail.com>
2021-01-06 16:41:01 +0100

    improved version of D5P1

--- plain.js
+++ plain.js
@@ -5,12 +5,11 @@ if(fs.existsSync(inputPath)){
     fs.readFile(inputPath, (err, data) => {
         if (err) console.log(err);
         else {
-            const input = data.toString().split("\r\n").map( element => { return { "row"    : Number.parseInt(element.slice(0,7 ).replace(/F/g, '0').replace(/B/g, '1'), 2), 
-                                                                                   "column" : Number.parseInt(element.slice(7,10).replace(/L/g, '0').replace(/R/g, '1'), 2) }});
+            const seatIds = data.toString().split("\r\n").map( element => Number.parseInt(element.replace(/F|L/g, '0').replace(/B|R/g, '1'), 2));
+
             let highestSeatId = 0;
-            input.forEach(element => {
-                const seatId = (element.row * 8) + element.column;
-                if (highestSeatId < seatId) highestSeatId = seatId;
+            seatIds.forEach(id => {
+                if (highestSeatId < id) highestSeatId = id;
             });
             console.log(highestSeatId);
         }
