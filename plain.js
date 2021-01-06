927ea7a61aef5e5e2a918d28144307d3b7e18d07
PukieDiederik <diederik.boeren@gmail.com>
2021-01-06 17:29:00 +0100

    ADD d5p2 and did some small refactoring

--- plain.js
+++ plain.js
@@ -6,12 +6,14 @@ if(fs.existsSync(inputPath)){
         if (err) console.log(err);
         else {
             const seatIds = data.toString().split("\r\n").map( element => Number.parseInt(element.replace(/F|L/g, '0').replace(/B|R/g, '1'), 2));
+            seatIds.sort((a,b)  => a - b); //sort for easy searching and figuring out the highest/lowest
 
-            let highestSeatId = 0;
-            seatIds.forEach(id => {
-                if (highestSeatId < id) highestSeatId = id;
-            });
-            console.log(highestSeatId);
+            //look for the missing seat
+            for(let i = 1; i < seatIds.length; i++){ if(seatIds[i] != i + seatIds[0]) { console.log("missing seatId: " + (i + seatIds[0])); break; } }
+
+            //print the highest and lowest seat
+            console.log("lowest SeatId: " +  seatIds[0]);
+            console.log("highest SeatId: " + seatIds[seatIds.length - 1]);
         }
     });
 } else console.error("[ERROR] Could not find file specified");
