9ff3f0ed25671d26c580a32c4d5da18adf4ccd8a
PukieDiederik <diederik.boeren@gmail.com>
2021-01-08 13:11:40 +0100

    d6 p2

--- survey.js
+++ survey.js
@@ -5,8 +5,13 @@ if(fs.existsSync(inputPath)){
     fs.readFile(inputPath, (err, data) => {
         if (err) console.log(err);
         else {
-            const surveys = data.toString().split(/\r\n\r\n/g).map(survey => survey.replace(/\r\n/g, ''));
-            let uniqueAnswerAmount = 0;
+            const surveys = data.toString().split(/\r\n\r\n/g); //split each group
+            const surveyGroupSize = surveys.map(survey => survey.match(/\r\n/g) ? survey.match(/\r\n/g).length + 1 : 1); //count amount of people in groups
+
+            let uniqueAnswerAmount = 0; //amount of unique answers in all groups
+            let allAnswerAmount = 0; //the amount of questions where everyone in the group said yes
+
+            //calculate all the unique answers
             const uniqueAnswers = surveys.map(survey => {
                 let uniqueSet = new Set();
                 let i = survey.length;
@@ -17,7 +22,15 @@ if(fs.existsSync(inputPath)){
                 return uniqueSet;
             });
 
-            console.log(uniqueAnswerAmount);
+            //check the amount of answers that everyone has in their group
+            for(let i = 0; i < surveys.length; i++) {
+                uniqueAnswers[i].forEach( letter => {
+                    allAnswerAmount += surveys[i].match(RegExp(letter, 'g')).length == surveyGroupSize[i];
+                });
+            }
+            
+            console.log("Unique answer amount: " + uniqueAnswerAmount);
+            console.log("All answer amount: " + allAnswerAmount);
         }
     });
 } else console.error("[ERROR] Could not find file specified");
